f8c85f881d97edeae5673d88921713c88ef52776
Suriya Kenma <bekzerker@gmail.com>
2025-08-13 23:01:32 +0700

    add - renderer

--- init_windows/main.c
+++ init_windows/main.c
@@ -1,3 +1,5 @@
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
@@ -12,6 +14,7 @@ int main(int argc, char *argv[]) {
     printf("Error Initializing SDL: %s\n", SDL_GetError());
     return 1;
   }
+
   SDL_Window *window =
       SDL_CreateWindow("Initialize Windows", SDL_WINDOWPOS_CENTERED,
                        SDL_WINDOWPOS_CENTERED, 640, 480, 0);
@@ -19,6 +22,17 @@ int main(int argc, char *argv[]) {
   if (!window) {
     printf("Error Creating Windows: %s\n", SDL_GetError());
   }
+
+  Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, render_flags);
+
+  if (!renderer) {
+    printf("Error Creating Renderer: %s\n", SDL_GetError());
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
   SDL_Delay(3000);
 
   // clean up resource before it closing
